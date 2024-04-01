7e783fd48cfb1da45488e51e92c63da321135f32
Charles Chan <rascov@gmail.com>
2024-04-01 15:21:20 -0700

    Introduce internal config pkg

--- cmd/arctic-gardener/main.go
+++ cmd/arctic-gardener/main.go
@@ -3,74 +3,43 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
+	"github.com/charlesmcchan/arctic-gardener/internal/config"
 	"github.com/stianeikeland/go-rpio/v4"
-	"gopkg.in/yaml.v2"
 )
 
-type Config struct {
-	Pins []int `yaml:"pins"`
-	PollIntervalMs int `yaml:"pollIntervalMs"`
-	WetThreshold int `yaml:"wetThreshold"`
-	DryThreshold int `yaml:"dryThreshold"`
-}
-
 func main() {
-	// Parse command-line arguments
 	configPath := flag.String("c", "configs/configs.yaml", "path to config file")
 	flag.Parse()
 
-	// Read config from file
-	configFile, err := ioutil.ReadFile(*configPath)
+	c, err := config.NewConfig(*configPath)
 	if err != nil {
-		fmt.Printf("Failed to read config file: %v\n", err)
+		fmt.Println("Config error:", err)
 		os.Exit(1)
 	}
 
-	// Parse config
-	var configs Config
-	if err := yaml.Unmarshal(configFile, &configs); err != nil {
-		fmt.Println("Failed to parse config:", err)
-		os.Exit(1)
-	}
-	if len(configs.Pins) == 0 {
-		fmt.Println("No pins specified in config")
-		os.Exit(1)
-	}
-	fmt.Printf("Config: %+v\n", configs)
-
-	// Open and map memory to access gpio, check for errors
 	if err := rpio.Open(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
-	// Unmap gpio memory when done
 	defer rpio.Close()
 
-	// Signal handling
-	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-	go func() {
-		sig := <- ch
-		fmt.Println("Received signal:", sig)
-		rpio.Close()
-		os.Exit(0)
-	}()
+	fmt.Printf("Config: %+v\n", c)
 
-	for {
-		fmt.Println("---")
-		for _, pin := range configs.Pins {
-			pin := rpio.Pin(pin)
-			pin.Input()
-			pin.PullDown()
-			fmt.Printf("Pin %d = %d\n", pin, pin.Read())
-		}
-		time.Sleep(time.Duration(configs.PollIntervalMs) * time.Millisecond)
+	pin := rpio.Pin(c.Pin.Sensor)
+	pin.Input()
+	pin.PullDown()
+	sensorResult := pin.Read()
+	fmt.Printf("Pin %d = %d\n", pin, sensorResult)
+
+	pin = rpio.Pin(c.Pin.Pump)
+	pin.Output()
+	if sensorResult == rpio.Low {
+		fmt.Println("Soil is wet")
+		pin.Low()
+	} else {
+		fmt.Println("Soil is dry")
+		pin.High()
 	}
 }
---
+++ internal/config/config.go
@@ -0,0 +1,65 @@
+package config
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Threshold struct {
+	Wet int `yaml:"wet"`
+	Dry int `yaml:"dry"`
+}
+
+type Pin struct {
+	Sensor int `yaml:"sensor"`
+	Pump int `yaml:"pump"`
+}
+
+// Config represents the configuration for the application.
+type Config struct {
+	Pin Pin `yaml:"pin"`
+	Threshold Threshold `yaml:"threshold"`
+}
+
+// Init reads the configuration from the provided file path
+// and returns a Config object.
+func NewConfig(configPath string) (Config, error) {
+	var config Config
+
+	configFile, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		fmt.Printf("Failed to read config file: %v\n", err)
+		return config, err
+	}
+
+	if err := yaml.Unmarshal(configFile, &config); err != nil {
+		fmt.Println("Failed to parse config:", err)
+		return config, err
+	}
+
+	if err = checkConfig(config) ; err != nil {
+		return config, err
+	}
+
+	return config, nil
+}
+
+// checkConfig validates the provided configuration.
+// If any validation fails, it returns an error.
+func checkConfig(c Config) error {
+	if c.Threshold.Wet < 0 || c.Threshold.Wet > 100 {
+		return fmt.Errorf("WetThreshold must be between 0 and 100")
+	}
+	if c.Threshold.Dry < 0 || c.Threshold.Dry > 100 {
+		return fmt.Errorf("DryThreshold must be between 0 and 100")
+	}
+	if c.Pin.Sensor == 0 {
+		return fmt.Errorf("Pins.Sensor must not be 0")
+	}
+	if c.Pin.Pump == 0 {
+		return fmt.Errorf("Pins.Pump must not be 0")
+	}
+	return nil
+}
